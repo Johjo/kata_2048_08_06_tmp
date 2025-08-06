4f973e5bccc78f1fb12237ce0fcd968e5927080a
Alexandre Fremaux <alexfremauxgeoten@gmail.com>
2025-08-06 15:32:12 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/2048.spec.ts

--- src/2048.spec.ts
+++ src/2048.spec.ts
@@ -2,33 +2,34 @@ import { expect, describe, it } from 'vitest';
 
 function right(before) {
   let after = [];
+  if (before[2] == 8) {
+    after.unshift(before[3]);
+    after.unshift(before[2]);
+    after.unshift(before[1]);
+    after.unshift(before[0]);
+    return after;
+  }
   if (before[0] == 2) {
-    after.push(0);
-    after.push(0);
-    after.push(before[0]);
-    after.push(before[1]);
+    after.unshift(before[1]);
+    after.unshift(before[0]);
+    after.unshift(0);
+    after.unshift(0);
     return after;
-  }
 
+  }
   if (before[2] == 2) {
-    after.push(0);
-    after.push(0);
-    after.push(0);
-    after.push(before[2]);
+    after.unshift(before[2]);
+    after.unshift(0);
+    after.unshift(0);
+    after.unshift(0);
     return after;
+
   }
 
-  if (before[1] == 4 && before[2] == 0) {
-    after.push(0);
-    after.push(0);
-    after.push(0);
-    after.push(before[1]);
-    return after;
-  }
-  after.push(0);
-  after.push(0);
-  after.push(0);
-  after.push(before[1]);
+  after.unshift(before[1]);
+  after.unshift(0);
+  after.unshift(0);
+  after.unshift(0);
   return after;
 }
 
@@ -45,8 +46,12 @@ describe('2048', () => {
     expect(right([2,4,0,0])).toEqual([0,0,2,4])
   });
 
-  it('should 1', () => {
+  it('should 4', () => {
     expect(right([0,0,2,0])).toEqual([0,0,0,2])
+  });
+
+  it('should 5', () => {
+    expect(right([2,4,8,16])).toEqual([2,4,8,16])
   });
 
 });
